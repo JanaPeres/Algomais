56a63b2b551cc9866f0d426ff2226ab5b2de7231
Janaina <janaina.peres01@etec.sp.gov.br>
2023-03-15 22:17:37 -0300

    continuacao

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\VestuariosController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/vestuarios', [VestuariosController::class, 'index'])->name('vestuarios.index');
+
+Route::get('/vestuarios/create', [VestuariosController::class, 'create'])->name('vestuarios.create');
+
+Route::get('/vestuarios/{id}', [VestuariosController::class, 'show'])->name('vestuarios.show');
+
+Route::get('/vestuarios/{id}/edit', [VestuariosController::class, 'edit'])->name('vestuarios.edit');
+
+Route::post('/vestuarios', [VestuariosController::class, 'store'])->name('vestuarios.store');
+
+Route::put('/vestuarios/{id}', [VestuariosController::class, 'update'])->name('vestuarios.update');
+
+Route::delete('/vestuarios/{id}', [VestuariosController::class, 'destroy'])->name('vestuarios.destroy');
