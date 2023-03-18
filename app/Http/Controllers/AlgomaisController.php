0699a3deb0a4239577a3e0da08962a5b37ec6bb4
Janaina <janaina.peres01@etec.sp.gov.br>
2023-03-17 21:46:13 -0300

    Adicionando funções no controller

--- app/Http/Controllers/AlgomaisController.php
+++ app/Http/Controllers/AlgomaisController.php
@@ -3,8 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Vestuario;
 
 class AlgomaisController extends Controller
 {
-    //
+    public function index()
+    {
+
+        $algomais = Vestuario::all();
+
+        return view('algomais.index', compact('vestuarios'));
+    }
+
+    public function create()
+    {
+
+    }
+    public function store(Request $request)
+    {
+
+    }
+
+    public function show($id)
+    {
+
+    }
+
+    public function edit($id)
+    {
+
+    }
+
+
+    public function update(Request $request, $id)
+    {
+
+    }
+
+    public function destroy($id)
+    {
+
+    }
+
 }
